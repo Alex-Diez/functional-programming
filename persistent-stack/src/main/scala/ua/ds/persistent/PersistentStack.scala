290c5d005ce398ae47671f86402a4fdd8dc2050a
Alex-Diez <alexvic.amatory@gmail.com>
2016-09-12 09:35:21 +0100

    add filter/foreach/toIterator methods to stack

--- persistent-stack/src/main/scala/ua/ds/persistent/PersistentStack.scala
+++ persistent-stack/src/main/scala/ua/ds/persistent/PersistentStack.scala
@@ -1,20 +1,60 @@
 package ua.ds.persistent
 
-trait PersistentStack {
-    def peek: Option[Int]
+trait Stack[T] {
+    def peek: Option[T]
 
     def size(): Int
 
-    def push(value: Int): PersistentStack = {
-        new ValueStack(size() + 1, Some(value), Some(this))
-    }
+    def push(value: T): Stack[T]
 
-    def pop(): Option[PersistentStack]
+    def pop(): Option[Stack[T]]
+
+    def toIterator: Iterator[T]
+
+    def foreach[A](mapperFunctor: T => A): Stack[A]
+
+    def filter(filterFunctor: (T) => Boolean): Stack[T]
 }
 
 object PersistentStack {
-    def apply(): PersistentStack = new ValueStack(0, None, None)
+    def apply[T](): PersistentStack[T] = new PersistentStack(0, None, None)
 }
 
-private class ValueStack(val size: Int, val peek: Option[Int], val pop: Option[PersistentStack]) extends PersistentStack {
+class PersistentStack[T](val size: Int, val peek: Option[T], val pop: Option[Stack[T]]) extends Stack[T] {
+    override def push(value: T): Stack[T] = {
+        new PersistentStack(size + 1, Some(value), Some(this))
+    }
+
+    override def toIterator: Iterator[T] = {
+        new StackIterator(this)
+    }
+
+    override def foreach[A](mapperFunctor: T => A): Stack[A] = {
+        new PersistentStack[A](size, peek.map(mapperFunctor), pop.map(s => s.foreach(mapperFunctor)))
+    }
+
+    override def filter(filterFunctor: (T) => Boolean): Stack[T] = {
+        pop match {
+            case Some(e) =>
+                val elem = e.filter(filterFunctor)
+                peek match {
+                    case Some(v) if filterFunctor(v) => new PersistentStack(elem.size + 1, Some(v), Some(elem))
+                    case _ => elem
+                }
+            case None => peek match {
+                case Some(v) if filterFunctor(v) => new PersistentStack(1, Some(v), None)
+                case None => PersistentStack()
+            }
+        }
+    }
+}
+
+private class StackIterator[T](var elem: Stack[T]) extends Iterator[T] {
+    override def hasNext: Boolean = elem.size() != 0
+
+    override def next(): T = {
+        val r = elem.peek.get
+        elem = elem.pop().get
+        r
+    }
 }
--- persistent-stack/src/test/scala/ua/ds/persistent/PersistentStackTest.scala
+++ persistent-stack/src/test/scala/ua/ds/persistent/PersistentStackTest.scala
@@ -7,23 +7,23 @@ import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 @RunWith(classOf[JUnitRunner])
 class PersistentStackTest extends FlatSpec with Matchers with BeforeAndAfter {
 
-    var emptyStack = PersistentStack()
+    var emptyStack = PersistentStack[Int]()
 
     before {
-        emptyStack = PersistentStack()
+        emptyStack = PersistentStack[Int]()
     }
 
     "An empty stack" should "have size 0" in {
         emptyStack.size shouldBe 0
     }
 
-    "A Stack's size" should "increase when push element into it" in {
+    "A stack's size" should "increase when push element into it" in {
         val stack = emptyStack.push(1)
 
         stack.size shouldBe 1
     }
 
-    "A Stack's size" should "decrease when pop element from it" in {
+    "A stack's size" should "decrease when pop element from it" in {
         val stack = emptyStack.push(10)
                 .push(20)
 
@@ -31,23 +31,48 @@ class PersistentStackTest extends FlatSpec with Matchers with BeforeAndAfter {
         stack.pop().get.size shouldBe 1
     }
 
-    "A Stack" should "peek value that was pushed into the stack" in {
+    "A stack" should "peek value that was pushed into the stack" in {
         val stack = emptyStack.push(10)
 
         stack.peek shouldBe Some(10)
     }
 
-    "Values" should "be pop in FIFO order" in {
-        val stack = PersistentStack().push(10)
+    "A stack" should "contain element in FIFO order" in {
+        val stack = emptyStack.push(10)
                 .push(20)
                 .push(30)
                 .push(40)
                 .push(50)
 
-        stack.peek shouldBe Some(50)
-        stack.pop().get.peek shouldBe Some(40)
-        stack.pop().get.pop().get.peek shouldBe Some(30)
-        stack.pop().get.pop().get.pop().get.peek shouldBe Some(20)
-        stack.pop().get.pop().get.pop().get.pop().get.peek shouldBe Some(10)
+        stack.toIterator.toStream should contain inOrderOnly (50, 40, 30, 20, 10)
+    }
+
+    "A stack" should "be created from existed one" in {
+        val stack = emptyStack.push(30)
+                .push(40)
+                .push(50)
+
+        val otherStack = stack.foreach(e => e * 2)
+
+        stack.toIterator.toStream should contain inOrderOnly (50, 40, 30)
+        otherStack.toIterator.toStream should contain inOrderOnly (100, 80, 60)
+    }
+
+    "A stack" should "be filtered" in {
+        val stack = emptyStack.push(20)
+                .push(25)
+                .push(30)
+                .push(35)
+                .push(40)
+                .push(45)
+                .push(50)
+
+        val filtered = stack.filter(e => e % 10 == 0)
+
+        filtered.toIterator.toStream should contain inOrderOnly (50, 40, 30, 20)
+    }
+
+    "A stack" should "" in {
+        pending
     }
 }
